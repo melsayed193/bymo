55fed999a47e980644fa8363edbc48ee6c059ede
melsayed193 <46494421+melsayed193@users.noreply.github.com>
2020-05-03 03:08:03 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,14 +4,17 @@ let contact = document.querySelector('.nav-contact')
 
 home.addEventListener('click', function() {
 	document.body.id = ''
+	document.body.classList.remove('menu-active')
 })
 
 work.addEventListener('click', function() {
 	document.body.id = 'work-active'
+	document.body.classList.remove('menu-active')
 })
 
 contact.addEventListener('click', function() {
 	document.body.id = 'contact-active'
+	document.body.classList.remove('menu-active')
 })
 
 
@@ -29,5 +32,11 @@ for (let i = 0; i < projects.length; i++) {
 		<div class="detail-image">${img}</div>`
 		detail.innerHTML = detailContent
 		document.querySelector('.work').id = 'detail-active'
+	})
+}
+
+if (document.querySelector('.menu-toggle')) {
+	document.querySelector('.menu-toggle').addEventListener('click', function() {
+		document.body.classList.toggle('menu-active')
 	})
 }
